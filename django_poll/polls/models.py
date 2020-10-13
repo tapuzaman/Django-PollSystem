7729648a10f54477f5da75dc888e5196406b2238
tapuzaman <tapu.zaman101@gmail.com>
2020-10-13 20:09:37 +0600

    database table created

--- django_poll/polls/models.py
+++ django_poll/polls/models.py
@@ -6,9 +6,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+ #to return the text not an object of question
+    def __str__(self):
+        return self.question_text
+
 #how question model will work
-    class Answer(models.Model):
-#making the relation with the question model using foreign key
+class Answer(models.Model):
+    #making the relation with the question model using foreign key
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    #to return the text not an object of answer 
+    def __str__(self):
+        return self.question_text
