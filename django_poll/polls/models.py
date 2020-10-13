c8443d36b285478449e459a21cb4012d9a68e3f2
tapuzaman <tapu.zaman101@gmail.com>
2020-10-13 19:48:17 +0600

    Question and Answer model created

--- django_poll/polls/models.py
+++ django_poll/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#how question model will work
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+#how question model will work
+    class Answer(models.Model):
+#making the relation with the question model using foreign key
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
