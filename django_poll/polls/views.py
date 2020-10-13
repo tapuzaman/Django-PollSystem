69c41ce17a82cb7d79fccb98d2af0a495e08efdc
tapuzaman <tapu.zaman101@gmail.com>
2020-10-13 23:47:02 +0600

    Project v1 completed

--- django_poll/polls/views.py
+++ django_poll/polls/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 # Create your views here.
+
+from .models import Question, Answer
+
+# Get questions and display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# Show specific question and answers
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', { 'question': question })
+
+# Get question and display results
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
+
+
+# Vote for a question answer
+def vote(request, question_id):
+    # print(request.POST['answer'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_answer = question.answer_set.get(pk=request.POST['answer'])
+    except (KeyError, Answer.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a answer.",
+        })
+    else:
+        selected_answer.votes += 1
+        selected_answer.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
